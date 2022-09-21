9a898c2a8ec2da147d7a3a5b43e9b7ca3f47d793
Jtellez19 <jtellez@deacero.com>
2022-09-20 20:43:21 -0500

    Archivo actualizado

---
+++ operaciones_edad/operaciones.py
@@ -0,0 +1,21 @@
+## Actividad 1--> 2x = 24
+## x = 2/24
+
+print("1.-El doble de mi edad tiene 24 anios, cuantos anios tengo? (x=?)")
+print("La ecuacion es 2x = 24")
+print("El resultado es: x = 24/2")
+
+edad1 = 24/2
+print("Edad = "+ str(x))
+
+print("A un tercio de la edad de mi hermana le disminuyo 15 anios. Tengo 6 anios. Que edad tiene? (Escribir la ecuacion correspondiente, despejar y calcular x de manera manual)")
+print("La ecuacion es: (1/3)(x)-15=6")
+print("El resultado es: x = 63")
+
+edad2 = 63
+
+if edad1 > edad2:
+    print("edad1 es mas grande")
+
+else:
+    print("edad2 es mas grande")
