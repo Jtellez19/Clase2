f8286e85734cd6d8826c3b3d9b96a0bed9e07283
Jtellez19 <jtellez@deacero.com>
2022-09-27 20:11:52 -0500

    Cambios a Compras.py

--- Compras/Compras.py
+++ Compras/Compras.py
@@ -7,5 +7,7 @@ while compras != 0:
         acumulado += compras
     elif compras < 0:
         print("Ingresa valores positivos")
-
+if acumulado >= 1000:
+    acumulado = acumulado-(acumulado*0.10)
+    print("Ha obtenido un descuento del 10%")
 print("La cantidad a pagar es de: "+str(acumulado))
